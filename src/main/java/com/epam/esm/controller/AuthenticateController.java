967fdb8267436f7c3275b6d3e7c45aa7139ba45c
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2023-05-08 12:47:21 +0300

    #21 tokens putted into header

--- src/main/java/com/epam/esm/controller/AuthenticateController.java
+++ src/main/java/com/epam/esm/controller/AuthenticateController.java
@@ -5,6 +5,9 @@ import com.epam.esm.model.DTO.login.LoginResponse;
 import com.epam.esm.security.UserPrincipal;
 import com.epam.esm.security.service.AuthenticateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +23,13 @@ public class AuthenticateController {
     private final AuthenticateService service;
 
     @GetMapping("/login")
-    public LoginResponse login(@RequestBody @Validated LoginRequest request){
-        return service.attemptLogin(request.getName(), request.getPassword());
+    public ResponseEntity<LoginResponse> login(@RequestBody @Validated LoginRequest request){
+        LoginResponse loginResponse = service.attemptLogin(request.getName(), request.getPassword());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access_token", loginResponse.getAccessToken());
+        headers.add("refresh_token", loginResponse.getRefreshToken());
+
+        return new ResponseEntity<>(loginResponse, headers, HttpStatus.OK);
     }
 
     @GetMapping("/secured")
