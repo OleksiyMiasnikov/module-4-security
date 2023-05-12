25de854702d3fa3b4d466fe74d1d347e03bea303
Alex <oleksiymiasnikov@gmail.com>
2023-05-12 19:45:16 +0300

    #30 AuthenticateControllerTest continued

--- src/test/java/com/epam/esm/controller/AuthenticateControllerTest.java
+++ src/test/java/com/epam/esm/controller/AuthenticateControllerTest.java
@@ -3,6 +3,7 @@ package com.epam.esm.controller;
 import com.epam.esm.controller.advice.ApiExceptionHandler;
 import com.epam.esm.model.DTO.login.LoginRequest;
 import com.epam.esm.model.DTO.login.LoginResponse;
+import com.epam.esm.security.UserPrincipal;
 import com.epam.esm.security.service.AuthenticateService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -68,7 +73,16 @@ class AuthenticateControllerTest {
     }
 
     @Test
-    void secured() {
+    void secured() throws Exception {
+        this.mockMvc.perform(get("/secured")
+                        .with(SecurityMockMvcRequestPostProcessors
+                                .user(UserPrincipal.builder()
+                                        .userId(1L)
+                                        .name("user_1")
+
+                                        .build())))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
