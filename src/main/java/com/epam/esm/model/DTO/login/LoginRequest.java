29f41af5d7c852a9cb41c7a5b0da55a2de5eda41
Alex <oleksiymiasnikov@gmail.com>
2023-05-12 19:29:46 +0300

    #29 AuthenticateControllerTest started

--- src/main/java/com/epam/esm/model/DTO/login/LoginRequest.java
+++ src/main/java/com/epam/esm/model/DTO/login/LoginRequest.java
@@ -1,10 +1,11 @@
 package com.epam.esm.model.DTO.login;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String name;
     private String password;
---
+++ src/test/java/com/epam/esm/controller/AuthenticateControllerTest.java
@@ -0,0 +1,77 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.controller.advice.ApiExceptionHandler;
+import com.epam.esm.model.DTO.login.LoginRequest;
+import com.epam.esm.model.DTO.login.LoginResponse;
+import com.epam.esm.security.service.AuthenticateService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class AuthenticateControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Mock
+    AuthenticateService service;
+    @InjectMocks
+    AuthenticateController subject;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(subject)
+                .setControllerAdvice(new ApiExceptionHandler())
+                .build();
+    }
+
+    @Test
+    void login() throws Exception {
+        LoginRequest request = LoginRequest.builder()
+                .name("user")
+                .password("111")
+                .build();
+        LoginResponse response = LoginResponse.builder()
+                .accessToken("- access token -")
+                .refreshToken("- refresh token -")
+                .build();
+
+        String jsonLogin = "{\"name\": \"user\",\"password\": \"111\"}";
+
+        when(service.attemptLogin(request.getName(), request.getPassword())).thenReturn(response);
+
+        this.mockMvc.perform(get("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonLogin))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().stringValues("access_token", "- access token -"))
+                .andExpect(header().stringValues("refresh_token", "- refresh token -"));
+
+        verify(service).attemptLogin(request.getName(), request.getPassword());
+    }
+
+    @Test
+    void secured() {
+    }
+
+    @Test
+    void refreshTokens() {
+    }
+}
