fd0217571adde52fd1472cf4df308715d53f4155
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2023-05-23 18:00:24 +0300

    #46 war

--- src/main/java/com/epam/esm/Module4SecurityApplication.java
+++ src/main/java/com/epam/esm/Module4SecurityApplication.java
@@ -2,12 +2,18 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Module4SecurityApplication {
+public class Module4SecurityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Module4SecurityApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Module4SecurityApplication.class);
+	}
 }
